b220721b48cdb40ec9f9efa40cd37176d8195157
Chadin Chaipornpisuth <ping47024@icloud.com>
2022-08-11 15:24:45 +0700

    add comfirm to set value

--- script.js
+++ script.js
@@ -6,10 +6,15 @@ function init() {
   });
 }
 
-const getCoor = () => {
-    const data = document.getElementsByClassName('ldmap_contextmenu_info')[0].innerText
-    const lat = /\d{1,2}\.\d+/.exec(data)[0]
-    const long = /(9|10)\d\.\d+/.exec(data)[0]
-    document.getElementById('lat').value = lat
-    document.getElementById('long').value = long
+const getCoor = async () => {
+    const data = await document.getElementsByClassName('ldmap_contextmenu_info')[0].innerText.replaceAll('\n','')
+
+    const numbers = /\d+\.\d+,\s\d+\.\d+/.exec(data)[0].substring(5)
+
+    const [lat,long] = numbers.split(", ")
+    console.log(lat,long)
+    if (confirm(`Do You Want To Set Coor : ${numbers}`)) {
+      document.getElementById('lat').value = lat
+      document.getElementById('long').value = long
+    }
 }
