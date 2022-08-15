bcc8cc557c844b8550be98cd75c09048e6763d0e
Chadin Chaipornpisuth <ping47024@icloud.com>
2022-08-15 14:55:43 +0700

    draggable

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ try {
   var marker = new longdo.Marker({lat:13.75, lon:100.5}, {
     icon: {
       url: 'pin_mark.png',
-      offset: { x: 12, y: 84}
-    },
+      offset: { x: 12, y: 84},
+    }    
   });
 }
 catch(err) {
@@ -30,9 +30,18 @@ function init() {
   map.Ui.Fullscreen.visible(false);
   map.Ui.Crosshair.visible(false);
   map.location({lat:13.75, lon:100.5},false)
+  map.bound({
+    "minLat": 0.371667073304601,
+    "minLon": 91.19457945227623,
+    "maxLat": 21.716946745375022,
+    "maxLon": 111.67309507727623
+  });
   
   // map.location(longdo.LocationMode.Geolocation);
   // map.Overlays.drop(marker)
+  // map.Event.bind('drag',function() {
+  //   console.log('drag')
+  // })
   map.Event.bind('click', function() {
     var mouseLocation = map.location(longdo.LocationMode.Pointer);
     const currentMapId = document.getElementById("currentMapId").innerText
@@ -43,24 +52,27 @@ function init() {
     document.getElementById(`lat${Id}`).value = mouseLocation.lat
     document.getElementById(`long${Id}`).value = mouseLocation.lon
     removeMarker();
-    var markerTemp = new longdo.Marker(mouseLocation)
+    var markerTemp = new longdo.Marker(mouseLocation, {
+      icon: {
+        url: 'pin_mark.png',
+      },
+      draggable :true,
+      weight: longdo.OverlayWeight.Top,      
+    })
+    map.Event.bind('overlayDrop',function() {
+      // console.log('overlayDrop')
+      var mouseLocation = map.location(longdo.LocationMode.Pointer);
+      var currentMapId = document.getElementById("currentMapId").innerText
+      // console.log(currentMapId)
+      var Id = currentMapId.replace(/[A-z]/g,"")
+      // console.log(Id)
+      // console.log(`Mark map${Id} on ${mouseLocation.lat},${mouseLocation.lon}`)
+      document.getElementById(`lat${Id}`).value = mouseLocation.lat
+      document.getElementById(`long${Id}`).value = mouseLocation.lon
+
+    })
     map.Overlays.add(markerTemp);
   });
-}
-
-const getCoor = async () => {
-    // const data = await document.getElementsByClassName('ldmap_contextmenu_info')[0].innerText.replaceAll('\n','')
-
-    // const numbers = /\d+\.\d+,\s\d+\.\d+/.exec(data)[0].substring(5)
-    
-    console.log(`Current Coor : ${map.location().lat},${map.location().lon}`)
-    // const [lat,long] = numbers.split(", ")
-    // console.log(lat,long)
-    // if (confirm(`Do You Want To Set map${Id} : ${map.location().lat},${map.location().lon}`)) {
-      // document.getElementById(`lat${Id}`).value = map.location().lat
-      // document.getElementById(`long${Id}`).value = map.location().lon
-    // }
-    marker.move({lat:map.location().lat, lon:map.location().lon}, true);
 }
 
 const removeMarker = () => {
@@ -68,11 +80,11 @@ const removeMarker = () => {
 }
 
 const ResetCoorId = (i) => {
-  if (confirm(`Do You Want To Set map${i}`)) {
+  // if (confirm(`Do You Want To Set map${i}`)) {
     if (confirm(`Move To last Location ?`)) {
       map.location({lat:document.getElementById(`lat${i}`).value, lon:document.getElementById(`long${i}`).value},false)
     }
     document.getElementById(`lat${i}`).value=null;
     document.getElementById(`long${i}`).value=null;
-  }
+  // }
 }
